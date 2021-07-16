5e558379aa85032602fd9f957f1cce5a6ee2a1f8
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-07-16 18:00:50 +0100

    first commit

---
+++ chat/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def index(request):
+
+    context = {
+
+    }
+    return render(request,'index.html',context)
+
+def room(request,room_name):
+    
+    context = {
+        'room_name' : room_name,
+    }
+    return render(request,'chatroom.html',context)
